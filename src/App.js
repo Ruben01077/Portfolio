0b06172036374c042fb2c8081be61bf5288dc943
Rub <melikyan740@gmail.com>
2023-02-05 23:01:00 -0800

    deploy ready

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './SCSS/index.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='left_div'>
+
+          <div className='name'>Name</div>
+          <div className='last_name'>Last Name</div>
+          <div className='status'>Programmer</div>
+          <div className='description'>Your Dreams Have Come True.
+            A Sudden Warm Rainstorm Washes Down In Sweet Hyphens.
+            He Is At His Desk.
+            Nothing Beats A Complete Sentence.
+          </div>
+
+        </div>
+
+        <div className='right_div'>
+
+          <img className='img' src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/rocky-mountain-morning-estes-park-colorado-gregory-ballos.jpg'></img>
+
+
+        </div>
       </header>
     </div>
   );
