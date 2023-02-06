7dff63ba42213d997dad2e7cf21ede3a801deecd
Rub <melikyan740@gmail.com>
2023-02-05 23:50:55 -0800

    timeLine add

--- src/App.js
+++ src/App.js
@@ -23,7 +23,81 @@ function App() {
 
 
         </div>
+
+
+
       </header>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="main-timeline">
+              <div class="timeline">
+                <a href="#" class="timeline-content">
+                  <div class="timeline-icon">
+                    <i class="fa fa-globe"></i>
+                  </div>
+                  <span class="timeline-year">2020</span>
+                  <h3 class="title">Web Designing</h3>
+                  <p class="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                  </p>
+                </a>
+              </div>
+              <div class="timeline">
+                <a href="#" class="timeline-content">
+                  <div class="timeline-icon">
+                    <i class="fa fa-rocket"></i>
+                  </div>
+                  <span class="timeline-year">2019</span>
+                  <h3 class="title">Web Development</h3>
+                  <p class="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                  </p>
+                </a>
+              </div>
+              <div class="timeline">
+                    <a href="#" class="timeline-content">
+                        <div class="timeline-icon">
+                            <i class="fa fa-globe"></i>
+                        </div>
+                        <span class="timeline-year">2020</span>
+                        <h3 class="title">Web Designing</h3>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                        </p>
+                    </a>
+                </div>
+                <div class="timeline">
+                    <a href="#" class="timeline-content">
+                        <div class="timeline-icon">
+                            <i class="fa fa-globe"></i>
+                        </div>
+                        <span class="timeline-year">2020</span>
+                        <h3 class="title">Web Designing</h3>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                        </p>
+                    </a>
+                </div>
+                <div class="timeline">
+                    <a href="#" class="timeline-content">
+                        <div class="timeline-icon">
+                            <i class="fa fa-globe"></i>
+                        </div>
+                        <span class="timeline-year">2020</span>
+                        <h3 class="title">Web Designing</h3>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                        </p>
+                    </a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
